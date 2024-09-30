44f5dfc4cf5ef0725037e70616a27f898282bcea
alexandresamson <alexander_samson@icloud.com>
2024-09-30 10:07:48 +0200

    Initial Commit

---
+++ Swift Lern App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift Lern App
+//
+//  Created by Alexandre Samson on 30.09.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift Lern App/Swift_Lern_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Lern_AppApp.swift
+//  Swift Lern App
+//
+//  Created by Alexandre Samson on 30.09.24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Lern_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
